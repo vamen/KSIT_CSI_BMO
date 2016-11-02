af3817dea8caeb2e6446bd99b371c46ec1eb435b
vamen <vivekstarstar@gmail.com>
2016-11-03 02:32:23 +0530

    framework built

---
+++ contest.php
@@ -0,0 +1,17 @@
+<?php
+echo "<h1>page is under construction</h1>"
+?>
+<html>
+<head>
+<title>Bug Me Out</title>
+</head>
+<body>
+<script language="javascript">
+document.write("hello from javascript<br>");
+</script>
+<?php
+echo "hello from php";
+?>
+
+</body>
+</html>
